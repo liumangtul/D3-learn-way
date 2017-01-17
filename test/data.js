3297c93a6a341b69eaedf807c7984209b73351f6
wangyan <promiseid@126.com>
2017-01-17 16:55:13 +0800

    修改示例

---
+++ test/data.js
@@ -0,0 +1,89 @@
+var treeData = [
+	{
+		"name": "中国",
+		"parent": "null",
+		"children": [
+			{
+				"name": "北京市",
+				"children": [
+					{
+						"name": "通州区",
+						"parent": "Architect",
+						"_children": [
+							{
+								"name":   "新华",
+								"url": "https://www.youtube.com/v/jrKA3cJmoms"
+							}
+
+						]
+					},
+					{
+						"name": "朝阳区",
+						"_children": [
+							{
+								"name":   "管庄",
+								"_children":[
+									{
+										"name":'神奇环岛',
+										"url": "https://www.youtube.com/v/17dr2GMvgd8",
+									}
+								]
+							},
+
+							{
+								"name":   "梆子井",
+								"url": "https://www.youtube.com/v/X3Hg6OXhPO8"
+							},
+							{
+								"name":   "双桥",
+								"url": "https://www.youtube.com/v/c1ccSXlVjXk"
+							}
+
+						]
+
+					},
+					{
+						"name": "海淀区",
+						"_children": [
+							{
+								"name":   "海淀黄庄",
+								"url": "https://www.youtube.com/v/MSp61khlP9s"
+							}
+						]
+
+					}
+
+				]
+			},
+			{
+				"name": "河北省",
+				"_children": [
+					{
+						"name": "燕郊",
+						"_children": [
+							{
+								"name":   "一区",
+								"url": "https://www.youtube.com/v/17dr2GMvgd8"
+							},
+
+							{
+								"name":   "二区",
+								"url": "https://www.youtube.com/v/X3Hg6OXhPO8"
+							},
+							{
+								"name":   "三区",
+								"url": "https://www.youtube.com/v/c1ccSXlVjXk"
+							}
+
+						]
+
+					}
+
+
+
+				]
+
+			}
+		]
+	}
+];
